352012dbf40944790adaa5a219aa0af0cf488b54
Raul Costa <raulbrennersc@gmail.com>
2020-07-30 13:16:42 -0300

    Criando o projeto e classe Game

---
+++ JogoDaVelha/JogoDaVelha.API/Entities/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha.API.Entities
+{
+    public class Game
+    {
+        public virtual char[][] Matrix { get; set; }
+        public virtual Guid Id { get; set; }
+        public virtual char NextPlayer { get; set; }
+    }
+}
